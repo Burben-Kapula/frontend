4462a4d85721ff1ef295c2b8080647377297125c
Burben <burbenburbenov@gmail.com>
2025-11-25 13:54:34 +0200

    test change to backup api

--- src/servise/persons.js
+++ src/servise/persons.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseUrl = 'https://for-public2.onrender.com/api/persons';
+const baseUrl = 'https://beckend-backup.onrender.com/';
 
 
 // Отримати всі контакти (GET)
